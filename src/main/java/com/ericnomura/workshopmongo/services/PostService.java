455f981c74dfa27fd1caafae0954653047b5b241
Eric Eiti Nomura <eric.nomura33@gmail.com>
2022-03-29 17:19:55 -0300

    Obtendo um post por id

---
+++ src/main/java/com/ericnomura/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.ericnomura.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ericnomura.workshopmongo.domain.Post;
+import com.ericnomura.workshopmongo.repository.PostRepository;
+import com.ericnomura.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
